54be893a89fe30aa64001b6c6a2e002e9ed4ed43
Jdonovan03 <54560973+Jdonovan03@users.noreply.github.com>
2019-10-13 22:37:43 -0400

    Add files via upload

---
+++ Page 60 Your Turn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import pandas as pd
+
+Location = "gradedata.csv"
+df = pd.read_csv(Location)
+
+df = df.sort_values(by=['lname', 'age', 'grade'], ascending=[True, True, True])
+df.head()
+
+
+
+# In[ ]:
+
+
+
+
